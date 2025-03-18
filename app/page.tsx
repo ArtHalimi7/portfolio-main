68b4e97fbc1f14456343c15fbde3be27f5f33f16
ArtHalimi7 <arthalimi989@gmail.com>
2025-03-18 14:27:10 +0100

    `Added Lenis library for smooth scrolling and updated dependencies`

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Hero from "../components/Hero";
 import Grid from "@/components/Grid";
@@ -6,17 +10,41 @@ import { navItems } from "@/data";
 import Experience from "@/components/Experience";
 import Approach from "@/components/Approach";
 import Footer from "@/components/Footer";
+
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: "vertical",
+      gestureOrientation: "vertical",
+      smoothWheel: true,
+      wheelMultiplier: 1,
+      touchMultiplier: 2,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems}/>
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
         <Experience />
         <Approach />
-        <Footer/>
+        <Footer />
       </div>
     </main>
   );
